cddf6fab7d1093f324c0e65a075642e8c738dcf2
u16orange <u16orange@gmail.com>
2022-07-17 04:57:29 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  
-  root to: 'toppages#index'
-  
   # root to: 'tasks#index'
 
+  root to: 'toppages#index'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :create]
+  
   resources :tasks
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_10_141736) do
+ActiveRecord::Schema.define(version: 2022_07_17_031019) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "status"
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
